d67f083eef8695b3ffc7fc9368fe3e9a33dff68e
Eduardo19910319 <eduardofabiano19@gmail.com>
2025-11-24 15:12:31 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Ignora erros de Typescript (acelera o build)
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    // Ignora erros de Linting (o erro que está te travando)
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+}
+module.exports = nextConfig
